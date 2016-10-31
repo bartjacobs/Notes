cf992fe3f0043134a74a2f58a604ebe8ed8937ee
Bart Jacobs <developer@bartjacobs.com>
2016-10-31 19:45:06 +0100

    Instantiate Core Data manager in ViewController

--- Notes/View Controllers/Root View Controller/ViewController.swift
+++ Notes/View Controllers/Root View Controller/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Properties
+
+    private let coreDataManager = CoreDataManager(modelName: "Notes")
+
     // MARK: - View Life Cycle
 
     override func viewDidLoad() {
