2380eb75322e47c92701c5dae1dccf68817d6459
Bart Jacobs <developer@bartjacobs.com>
2016-10-31 18:32:10 +0100

    Project setup

---
+++ Notes/View Controllers/Root View Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Bart Jacobs on 31/10/16.
+//  Copyright © 2016 Cocoacasts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
