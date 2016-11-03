e4441be3347352df3e6741609fd9b83d1fb87da0
Bart Jacobs <developer@bartjacobs.com>
2016-11-03 14:11:49 +0100

    Create extension for UIColor with convenience class methods

---
+++ Notes/Extensions/UIColor.swift
@@ -0,0 +1,17 @@
+//
+//  UIColor.swift
+//  Notes
+//
+//  Created by Bart Jacobs on 03/11/16.
+//  Copyright © 2016 Cocoacasts. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+
+    class func bitterSweet() -> UIColor {
+        return UIColor(red:0.99, green:0.47, blue:0.44, alpha:1.0)
+    }
+
+}
