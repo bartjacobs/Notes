d651d1fdc3586de6a29686ba9085b0a18c6610ed
Bart Jacobs <developer@bartjacobs.com>
2016-11-05 09:21:55 +0100

    Implement convenience methods to sort and format tags

--- Notes/Core Data/Extensions/Note.swift
+++ Notes/Core Data/Extensions/Note.swift
@@ -10,6 +10,8 @@ import Foundation
 
 extension Note {
 
+    // MARK: - Dates
+
     var updatedAtAsDate: Date {
         guard let updatedAt = updatedAt else { return Date() }
         return Date(timeIntervalSince1970: updatedAt.timeIntervalSince1970)
@@ -18,6 +20,29 @@ extension Note {
     var createdAtAsDate: Date {
         guard let createdAt = createdAt else { return Date() }
         return Date(timeIntervalSince1970: createdAt.timeIntervalSince1970)
+    }
+
+    // MARK: - Tags
+
+    var alphabetizedTags: [Tag]? {
+        guard let tags = tags as? Set<Tag> else {
+            return nil
+        }
+
+        return tags.sorted(by: {
+            guard let tag0 = $0.name else { return true }
+            guard let tag1 = $1.name else { return true }
+            return tag0 < tag1
+        })
+    }
+
+    var alphabetizedTagsAsString: String? {
+        guard let tags = alphabetizedTags, tags.count > 0 else {
+            return nil
+        }
+
+        let names = tags.flatMap { $0.name }
+        return names.joined(separator: ", ")
     }
 
 }
